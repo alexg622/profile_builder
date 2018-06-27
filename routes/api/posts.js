b33477b7e007507b1d4951925187fa578480d393
alexg622 <alex.gm62288@gmail.com>
2018-06-26 17:13:57 -0700

    Add like and unlike

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -57,7 +57,7 @@ router.post('/', passport.authenticate('jwt', { session: false }), (req, res) =>
   newPost.save().then(post => res.json(post));
 });
 
-// @route Dekete api/posts/:id
+// @route Delete api/posts/:id
 // @desc delete the post
 // @access private
 
@@ -75,6 +75,60 @@ router.delete('/:id', passport.authenticate('jwt', { session: false }), (req, re
       .catch(err => res.status(404).json({ postnotfound: 'No post found' }))
   })
 })
+
+// @route post api/posts/like/:id
+// @desc like post
+// @access private
+
+router.post('/like/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+  Profile.findOne({ user: req.user.id })
+  .then(profile => {
+    Post.findById(req.params.id)
+      .then(post => {
+        if(post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
+          return res.status(400).json({ alreadyliked: 'User already liked this post'});
+        }
+        // add user id to likes array
+        post.likes.unshift({ user: req.user.id });
+
+        post.save().then(post => res.json(post));
+      })
+      .catch(err => res.status(404).json({ postnotfound: 'No post found' }))
+  })
+})
+
+
+// @route post api/posts/unlike/:id
+// @desc unlike
+// @access private
+
+router.post('/unlike/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+  Profile.findOne({ user: req.user.id })
+  .then(profile => {
+    Post.findById(req.params.id)
+      .then(post => {
+        if(post.likes.filter(like => like.user.toString() === req.user.id).length === 0) {
+          return res.status(400).json({ notliked: 'You have not yet liked this post'});
+        }
+        // get remove index
+        const removeIndex = post.likes
+          .map(item => item.user.toString())
+          .indexOf(req.user.id);
+
+        // slice out of array
+        post.likes.splice(removeIndex, 1);
+        // save
+        post.save().then(post => res.json(post))
+      })
+      .catch(err => res.status(404).json({ postnotfound: 'No post found' }))
+  })
+})
+
+
+
+
+
+
 
 
 
